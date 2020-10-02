2f70953ad3670c88e2532eff3f93b8f233ffa2b8
Satrat <sara.adkins65@gmail.com>
2020-10-01 22:42:14 -0400

    demo updates

--- stream_analyze_game.py
+++ stream_analyze_game.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import OSC
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 c = OSC.OSCClient()
 c.connect(('127.0.0.1', 8000))
 print "OSC Connected"
@@ -15,26 +15,37 @@ upper = np.array(upper, dtype = "uint8")
 
 # set parameters for blob detection
 params = cv2.SimpleBlobDetector_Params()
+paramsB = cv2.SimpleBlobDetector_Params()
  
 # Change thresholds
 params.minThreshold = 10
 params.maxThreshold = 1000
+paramsB.minThreshold = 10
+paramsB.maxThreshold = 1000
  
 # Filter by Area.
 params.filterByArea = True
 params.minArea = 300
+paramsB.filterByArea = True
+paramsB.minArea = 300
  
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.1
+params.minCircularity = 0.8
+paramsB.filterByCircularity = True
+paramsB.minCircularity = 0.1
 
  
 # Filter by Convexity
 params.filterByConvexity = True
 params.minConvexity = 0.1
+paramsB.filterByConvexity = True
+paramsB.minConvexity = 0.1
  
 params.filterByColor = True
 params.blobColor = 255
+paramsB.filterByColor = True
+paramsB.blobColor = 255
 
 ret, image = cap.read()
 
@@ -52,7 +63,7 @@ count = 0
 
 ####SET THESE######
 size = 13
-slack = 12
+slack = 15
 ###################
 
 whites = np.zeros((size,size))
@@ -92,7 +103,6 @@ y1 = (uly + ury) / 2
 y2 = (lly + lry) / 2
 
 print x1,x2,y1,y2
-#boardSize = 12.
 tileX = (x2 - x1) / (size - 1)
 tileY = (y2 - y1) / (size - 1)
 
@@ -104,7 +114,7 @@ for i in xrange(size):
 		x = x1 + j * tileX
 		points.append((x,y))
 
-cv2.namedWindow('Keypoints', cv2.WINDOW_NORMAL)
+cv2.namedWindow('Lets Go', cv2.WINDOW_NORMAL)
 
 
 while(True):
@@ -122,9 +132,10 @@ while(True):
 	maskB = cv2.inRange(grayB, lower, upper)
 	outputB = cv2.bitwise_and(grayB, grayB, mask = maskB)
 	detector = cv2.SimpleBlobDetector_create(params)
+	detectorB = cv2.SimpleBlobDetector_create(paramsB)
 
 	keypoints = detector.detect(output)
-	keypointsB = detector.detect(outputB)
+	keypointsB = detectorB.detect(outputB)
 
 	white_ons = ""
 	black_ons = ""
@@ -137,10 +148,11 @@ while(True):
 			x = keyPoint.pt[0]
 			y = keyPoint.pt[1]
 			(xp, yp) = points[index]
+			#print row, col
 			if abs(x - xp) < slack and abs(y - yp) < slack:
 				whites[row][col] = 1.
 				flagW = True
-				print x,y
+				#print row,col, "GOOD"
 				white_ons += (str(col) + " " + str(row) + " 1 ")
 				print "WHITE"
 				print whites
@@ -155,10 +167,10 @@ while(True):
 			if abs(x - xp) < slack and abs(y - yp) < slack:
 				blacks[row][col] = 1.
 				flagB = True
-				print x,y
+				#print row,col
 				black_ons += (str(col) + " " + str(row) + " 1 ")
-				print "BLACK"
-				print blacks
+				#print "BLACK"
+				#print blacks
 		if(flagB == False and blacks[row][col] == 1.):
 			blacks[row][col] = 0.
 			black_ons += (str(col) + " " + str(row) + " 0 ")
@@ -166,7 +178,7 @@ while(True):
 	im_with_keypoints = cv2.drawKeypoints(output, keypoints, np.array([]), (255,0,255), 2)
 	im_with_keypointsB = cv2.drawKeypoints(outputB, keypointsB, np.array([]), (255,0,255), 2)
 
-	cv2.imshow("Keypoints", np.hstack([im_with_keypoints, im_with_keypointsB]))
+	cv2.imshow("Lets Go", np.hstack([im_with_keypoints, im_with_keypointsB]))
 
 	# send white token locations to Max over OSC
 	if(len(white_ons) > 0):
@@ -183,7 +195,7 @@ while(True):
 		c.send(blackMsg)
 
 	# Exit game
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	if cv2.waitKey(1000) & 0xFF == ord('q'):
 		break       
 
 # Clean up
